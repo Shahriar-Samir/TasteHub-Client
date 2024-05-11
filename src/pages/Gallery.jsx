cdd9c886a675808c7e977aee27bcc7966226735b
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-11 21:40:37 +0600

    added gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Gallery = () => {
     return (
         <div>
-            Gallery
+              <div className="bg-[linear-gradient(to_top,rgba(0,0,0,0.4),rgba(0,0,0,0)),linear-gradient(to_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url('/images/foodGallery.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-10 h-[70vh]">
+                <h1 className='text-5xl font-bold'>Gallery</h1>
+            </div>
         </div>
     );
 };
