3de498c04dc44f91c91e8936d184c268868c116e
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-11 12:20:40 +0600

    added all food items page's title section

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,15 +1,17 @@
+import Lottie from 'lottie-react';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+import AllFoodItems from '../../public/animations/allFoods.json'
 
 const AllFoods = () => {
     return (
         <div>
             <ToastContainer/>
-            <div className="bg-[linear-gradient(to_top,rgba(0,0,0,0.4),rgba(0,0,0,0)),linear-gradient(to_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url('/images/loginBanner.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-10 h-[100vh]">
+            <div className="bg-[linear-gradient(to_top,rgba(0,0,0,0.4),rgba(0,0,0,0)),linear-gradient(to_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url('/images/allFoods.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-10 h-[80vh]">
                 <h1 className='text-5xl font-bold'>All Food Items</h1>
             </div>
             <div>
-                
+
             </div>
         </div>
     );
