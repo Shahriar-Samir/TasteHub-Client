00e6f25eaecbf2136a999d5390d2d4b5502e06f9
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-11 21:17:03 +0600

    fecthed all my food items data from serverside and added them to cards

--- src/main.jsx
+++ src/main.jsx
@@ -57,7 +57,7 @@ const router = createBrowserRouter([{
         },
         {
           path: '/myFoodItems',
-          element: <MyFoodItems/>
+          element: <PrivateRoute><MyFoodItems/></PrivateRoute>,
         },
         {
           path: '/myPurchases',
--- src/pages/MyFoodItems.jsx
+++ src/pages/MyFoodItems.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+import Lottie from 'lottie-react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import Loading from './Loading';
+
 
 const MyFoodItems = () => {
-    // const {data} = useLoaderData()
+    const [foodsData,setFoodsData] = useState([])
+    const [loading,setLoading] = useState(true)
+    const {userLoggedIn} = useContext(AuthContext)
+    useEffect(()=>{
+        setLoading(true)
+        axios.get(`http://localhost:5000/myFoods/${userLoggedIn.email}`)
+        .then(res=>{
+            setLoading(false)
+            setFoodsData(res.data)
+        })
+        .catch(()=>{setLoading(false)})
+    },[])
+
     return (
         <div>
-            <h1 className='mt-40 text-center text-3xl font-bold'>My Food Items</h1>
+           {loading? <div className='h-[100vh] flex justify-center items-center'>
+                <Loading/>
+           </div> : <div> <h1 className='mt-40 text-center text-3xl font-bold'>My Food Items</h1>
             <div className='grid grid-cols-3 mx-auto w-11/12 max-w-[1200px] mt-10 gap-10'>
-                {[1,2,3,4].map(item=>{
-                    return <FoodItem/>
+                {foodsData.map(item=>{
+                    return <FoodItem key={item._id} item={item}/>
                 })}
-            </div>  
+            </div>  </div>}
         </div>
     );
 };
@@ -19,12 +37,13 @@ export default MyFoodItems;
 
 // food item card 
 
-const FoodItem = ()=>{
+const FoodItem = ({item})=>{
+    const {foodName,foodImage,} = item
     return(
         <div className="card shadow-xl">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src={foodImage} /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
+    <h2 className="card-title">{foodName}</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
