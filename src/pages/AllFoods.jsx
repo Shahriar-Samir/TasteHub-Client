6414fbf6fc1e4e6e1980fa8f93583c1468b97ff6
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-13 01:41:32 +0600

    added search food item functionalityon all foods page

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,10 +1,28 @@
 
+import { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast} from 'react-toastify';
+import axios from 'axios'
 
 
 const AllFoods = () => {
     const {data} = useLoaderData()
+    const [foodItems,setFoodItems] = useState(data)
+    const [resultTitle,setResultTitle] = useState('Results for : All')
+
+    const searchItems = (e)=>{
+            e.preventDefault()
+            const searchValue = e.target.search.value
+            setResultTitle(`Results for : ${searchValue? searchValue : 'All'}`)
+            axios.get(`http://localhost:5000/searchFood/${searchValue===''? 'false' : searchValue}`)
+            .then(res=>{
+                setFoodItems(res.data)
+            })
+            .catch(err=>{
+                toast.error('Something went wrong')
+            })
+    }
+    
     return (
         <div>
            <ToastContainer toastStyle={{backgroundColor:'#00000080',color:'white'}}/>
@@ -13,7 +31,7 @@ const AllFoods = () => {
             </div>
 
             <div className='mt-14'>
-            <form className="max-w-md mx-auto">   
+            <form className="max-w-md mx-auto" onSubmit={searchItems}>   
                 <label htmlFor="default-search" className="mb-2 text-sm font-medium text-white sr-only ">Search</label>
                 <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -21,18 +39,20 @@ const AllFoods = () => {
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" placeholder="Search food items" required name='searchFood' />
-                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-[#C90B12] hover:bg-[#8e282b] font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
+                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" placeholder="Search food items" name='search' />
+                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-[#C90B12] hover:bg-[#8e282b] font-medium rounded-lg text-sm px-4 py-2" >Search</button>
                 </div>
             </form>
             <div className='w-11/12 max-w-[1200px] mx-auto mt-5'>
-                <p className='text-center'>Results for : {<em>Food</em>}</p>
+                <p className='text-center'>{resultTitle}</p>
             </div>
-            <div className='w-11/12 mx-auto max-w-[1200px] grid grid-cols-3 gap-10 mt-5'>
-                {data.map(item=>{
+            {foodItems.length > 0 ? <div className='w-11/12 mx-auto max-w-[1200px] grid grid-cols-3 gap-10 mt-10'>
+                {foodItems.map(item=>{
                     return <Card key={item._id} item={item}/>
                 })}
-            </div>
+            </div> : <div className='h-[80vh] mt-10'>
+            <p className='text-xl text-center'>No items found</p>
+                </div>}
             </div>
         </div>
     );
